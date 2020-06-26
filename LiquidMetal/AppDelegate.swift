2c73f1c402295d90f820bce6a85f64e1c8825e0b
David Garcia <david_r_garcia@homedepot.com>
2020-06-26 15:34:01 -0400

    Initial Commit

---
+++ LiquidMetal/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LiquidMetal
+//
+//  Created by David Garcia on 6/26/20.
+//  Copyright © 2020 Wodinga. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LiquidMetal/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LiquidMetal
+//
+//  Created by David Garcia on 6/26/20.
+//  Copyright © 2020 Wodinga. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
